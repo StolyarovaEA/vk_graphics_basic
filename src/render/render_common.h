db5f8deb458e230c1ae0d31edcc12afeb552be41
StolyarovaEA <47775985+StolyarovaEA@users.noreply.github.com>
2023-06-07 22:17:07 +0300

    wroks

--- src/render/render_common.h
+++ src/render/render_common.h
@@ -9,17 +9,22 @@
 
 struct AppInput
 {
-  AppInput(){
+  AppInput()
+  {
     cams[1].pos    = LiteMath::float3(4.0f, 4.0f, 4.0f);
     cams[1].lookAt = LiteMath::float3(0, 0, 0);
     cams[1].up     = LiteMath::float3(0, 1, 0);
   }
 
-  enum {MAXKEYS = 384};
+  enum { MAXKEYS = 384 };
   Camera cams[2];
-  bool   keyPressed[MAXKEYS]{};
-  bool   keyReleased[MAXKEYS]{};
-  void clearKeys() { memset(keyPressed, 0, MAXKEYS*sizeof(bool)); memset(keyReleased, 0, MAXKEYS*sizeof(bool)); }
+  bool keyPressed[MAXKEYS]{};
+  bool keyReleased[MAXKEYS]{};
+  void clearKeys()
+  {
+    memset(keyPressed, 0, MAXKEYS * sizeof(bool));
+    memset(keyReleased, 0, MAXKEYS * sizeof(bool));
+  }
 };
 
 struct pipeline_data_t
@@ -28,8 +33,7 @@ struct pipeline_data_t
   VkPipeline pipeline;
 };
 
-enum class DrawMode
-{
+enum class DrawMode {
   WITH_GUI,
   NO_GUI
 };
@@ -37,20 +41,19 @@ enum class DrawMode
 class IRender
 {
 public:
-  virtual uint32_t     GetWidth() const = 0;
-  virtual uint32_t     GetHeight() const = 0;
-  virtual VkInstance   GetVkInstance() const = 0;
+  virtual uint32_t GetWidth() const        = 0;
+  virtual uint32_t GetHeight() const       = 0;
+  virtual VkInstance GetVkInstance() const = 0;
 
-  virtual void InitVulkan(const char** a_instanceExtensions, uint32_t a_instanceExtensionsCount, uint32_t a_deviceId) = 0;
-  virtual void InitPresentation(VkSurfaceKHR& a_surface, bool initGUI) = 0;
-  virtual void ProcessInput(const AppInput& input) = 0;
-  virtual void UpdateCamera(const Camera* cams, uint32_t a_camsCount) = 0;
-  virtual Camera GetCurrentCamera() { return { };};
-  virtual void LoadScene(const char* path, bool transpose_inst_matrices) = 0;
-  virtual void DrawFrame(float a_time, DrawMode a_mode) = 0;
+  virtual void InitVulkan(const char **a_instanceExtensions, uint32_t a_instanceExtensionsCount, uint32_t a_deviceId) = 0;
+  virtual void InitPresentation(VkSurfaceKHR &a_surface, bool initGUI)                                                = 0;
+  virtual void ProcessInput(const AppInput &input)                                                                    = 0;
+  virtual void UpdateCamera(const Camera *cams, uint32_t a_camsCount)                                                 = 0;
+  virtual Camera GetCurrentCamera() { return {}; };
+  virtual void LoadScene(const char *path, bool transpose_inst_matrices) = 0;
+  virtual void DrawFrame(float a_time, float a_dt, DrawMode a_mode)      = 0;
 
   virtual ~IRender() = default;
-
 };
 
-#endif//CHIMERA_RENDER_COMMON_H
+#endif// CHIMERA_RENDER_COMMON_H
--- src/utils/glfw_window.cpp
+++ src/utils/glfw_window.cpp
@@ -1,7 +1,7 @@
 #include "glfw_window.h"
 
 #ifdef WIN32
-#pragma comment(lib,"glfw3.lib")
+#pragma comment(lib, "glfw3.lib")
 #endif
 #include <iostream>
 #include <stdexcept>
@@ -21,8 +21,8 @@ constexpr bool g_enableValidationLayers = true;
 
 struct input_sample
 {
-  bool firstMouse   = true;
-  bool captureMouse = false;
+  bool firstMouse           = true;
+  bool captureMouse         = false;
   bool capturedMouseJustNow = false;
   bool wireframeMode        = false;
 
@@ -34,35 +34,35 @@ struct input_sample
 
 AppInput g_appInput;
 
-void onKeyboardPressedBasic(GLFWwindow* window, int key, int, int action, int)
+void onKeyboardPressedBasic(GLFWwindow *window, int key, int, int action, int)
 {
   switch (key)
   {
-    case GLFW_KEY_ESCAPE:
-      if (action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, VK_TRUE);
-      break;
-    case GLFW_KEY_LEFT_SHIFT:
-      g_inputDesktop.camMoveSpeed = 10.0f;
-      break;
+  case GLFW_KEY_ESCAPE:
+    if (action == GLFW_PRESS)
+      glfwSetWindowShouldClose(window, VK_TRUE);
+    break;
+  case GLFW_KEY_LEFT_SHIFT:
+    g_inputDesktop.camMoveSpeed = 10.0f;
+    break;
 
-    case GLFW_KEY_LEFT_CONTROL:
-      g_inputDesktop.camMoveSpeed = 1.0f;
-      break;
+  case GLFW_KEY_LEFT_CONTROL:
+    g_inputDesktop.camMoveSpeed = 1.0f;
+    break;
 
-    default:
-      if(key >= 0 && key < AppInput::MAXKEYS)
-      {
-        if (action == GLFW_RELEASE)
-          g_appInput.keyReleased[key] = true;
-        else if(action == GLFW_PRESS)
-          g_appInput.keyPressed[key] = true;
-      }
-      break;
+  default:
+    if (key >= 0 && key < AppInput::MAXKEYS)
+    {
+      if (action == GLFW_RELEASE)
+        g_appInput.keyReleased[key] = true;
+      else if (action == GLFW_PRESS)
+        g_appInput.keyPressed[key] = true;
+    }
+    break;
   }
 }
 
-void onMouseButtonClickedBasic(GLFWwindow* window, int button, int action, int)
+void onMouseButtonClickedBasic(GLFWwindow *window, int button, int action, int)
 {
   if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_RELEASE)
     g_inputDesktop.captureMouse = !g_inputDesktop.captureMouse;
@@ -74,10 +74,9 @@ void onMouseButtonClickedBasic(GLFWwindow* window, int button, int action, int)
   }
   else
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-
 }
 
-void onMouseMoveBasic(GLFWwindow*, double xpos, double ypos)
+void onMouseMoveBasic(GLFWwindow *, double xpos, double ypos)
 {
   if (g_inputDesktop.firstMouse)
   {
@@ -86,14 +85,14 @@ void onMouseMoveBasic(GLFWwindow*, double xpos, double ypos)
     g_inputDesktop.firstMouse = false;
   }
 
-//  float xoffset = float(xpos) - g_inputDesktop.lastX;
-//  float yoffset = g_inputDesktop.lastY - float(ypos);
+  //  float xoffset = float(xpos) - g_inputDesktop.lastX;
+  //  float yoffset = g_inputDesktop.lastY - float(ypos);
 
   g_inputDesktop.lastX = float(xpos);
   g_inputDesktop.lastY = float(ypos);
 }
 
-void onMouseScrollBasic(GLFWwindow*, double, double yoffset)
+void onMouseScrollBasic(GLFWwindow *, double, double yoffset)
 {
   g_inputDesktop.scrollY = float(yoffset);
 }
@@ -102,9 +101,9 @@ void onMouseScrollBasic(GLFWwindow*, double, double yoffset)
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void UpdateCamera(GLFWwindow* a_window, Camera& a_cam, float secondsElapsed)
+void UpdateCamera(GLFWwindow *a_window, Camera &a_cam, float secondsElapsed)
 {
-  //move position of camera based on WASD keys, and FR keys for up and down
+  // move position of camera based on WASD keys, and FR keys for up and down
   if (glfwGetKey(a_window, 'S'))
     a_cam.offsetPosition(secondsElapsed * g_inputDesktop.camMoveSpeed * -1.0f * a_cam.forward());
   else if (glfwGetKey(a_window, 'W'))
@@ -120,33 +119,32 @@ void UpdateCamera(GLFWwindow* a_window, Camera& a_cam, float secondsElapsed)
   else if (glfwGetKey(a_window, 'R'))
     a_cam.offsetPosition(secondsElapsed * g_inputDesktop.camMoveSpeed * a_cam.up);
 
-  //rotate camera based on mouse movement
+  // rotate camera based on mouse movement
   //
   if (g_inputDesktop.captureMouse)
   {
-    if(g_inputDesktop.capturedMouseJustNow)
+    if (g_inputDesktop.capturedMouseJustNow)
       glfwSetCursorPos(a_window, 0, 0);
 
     double mouseX, mouseY;
     glfwGetCursorPos(a_window, &mouseX, &mouseY);
     a_cam.offsetOrientation(g_inputDesktop.mouseSensitivity * float(mouseY), g_inputDesktop.mouseSensitivity * float(mouseX));
-    glfwSetCursorPos(a_window, 0, 0); //reset the mouse, so it doesn't go out of the window
+    glfwSetCursorPos(a_window, 0, 0);// reset the mouse, so it doesn't go out of the window
     g_inputDesktop.capturedMouseJustNow = false;
   }
 
-  //increase or decrease field of view based on mouse wheel
+  // increase or decrease field of view based on mouse wheel
   //
   const float zoomSensitivity = -0.2f;
-  float fieldOfView = a_cam.fov + zoomSensitivity * (float)g_inputDesktop.scrollY;
-  if(fieldOfView < 1.0f) fieldOfView   = 1.0f;
-  if(fieldOfView > 180.0f) fieldOfView = 180.0f;
+  float fieldOfView           = a_cam.fov + zoomSensitivity * (float)g_inputDesktop.scrollY;
+  if (fieldOfView < 1.0f) fieldOfView = 1.0f;
+  if (fieldOfView > 180.0f) fieldOfView = 180.0f;
   a_cam.fov = fieldOfView;
 
   g_inputDesktop.scrollY = 0.0f;
 }
 
-GLFWwindow* initWindow(int width, int height, GLFWkeyfun keyboard, GLFWcursorposfun mouseMove,
-                       GLFWmousebuttonfun mouseBtn, GLFWscrollfun mouseScroll)
+GLFWwindow *initWindow(int width, int height, GLFWkeyfun keyboard, GLFWcursorposfun mouseMove, GLFWmousebuttonfun mouseBtn, GLFWscrollfun mouseScroll)
 {
   glfwInit();
 
@@ -155,17 +153,17 @@ GLFWwindow* initWindow(int width, int height, GLFWkeyfun keyboard, GLFWcursorpos
 
   GLFWwindow *window = glfwCreateWindow(width, height, "GLFW window", nullptr, nullptr);
 
-  glfwSetKeyCallback        (window, keyboard);
-  glfwSetCursorPosCallback  (window, mouseMove);
+  glfwSetKeyCallback(window, keyboard);
+  glfwSetCursorPosCallback(window, mouseMove);
   glfwSetMouseButtonCallback(window, mouseBtn);
-  glfwSetScrollCallback     (window, mouseScroll);
-  //glfwSetInputMode          (window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+  glfwSetScrollCallback(window, mouseScroll);
+  // glfwSetInputMode          (window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
   glfwSwapInterval(0);
 
   return window;
 }
 
-void setupImGuiContext(GLFWwindow* a_window)
+void setupImGuiContext(GLFWwindow *a_window)
 {
   IMGUI_CHECKVERSION();
   ImGui::CreateContext();
@@ -183,45 +181,45 @@ void setupImGuiContext(GLFWwindow* a_window)
 }
 
 
-void mainLoop(std::shared_ptr<IRender> &app, GLFWwindow* window, bool displayGUI)
+void mainLoop(std::shared_ptr<IRender> &app, GLFWwindow *window, bool displayGUI)
 {
   constexpr int NAverage = 60;
-  double avgTime = 0.0;
-  int avgCounter = 0;
-  int currCam    = 0;
+  double avgTime         = 0.0;
+  int avgCounter         = 0;
+  int currCam            = 0;
 
   g_appInput.cams[0] = app->GetCurrentCamera();
-  double lastTime = glfwGetTime();
+  double lastTime    = glfwGetTime();
   while (!glfwWindowShouldClose(window))
   {
     double thisTime = glfwGetTime();
     double diffTime = thisTime - lastTime;
     lastTime        = thisTime;
-    
+
     g_appInput.clearKeys();
     glfwPollEvents();
-    
-    if(g_appInput.keyReleased[GLFW_KEY_L])
+
+    if (g_appInput.keyReleased[GLFW_KEY_L])
       currCam = 1 - currCam;
 
     UpdateCamera(window, g_appInput.cams[currCam], static_cast<float>(diffTime));
-    
+
     app->ProcessInput(g_appInput);
     app->UpdateCamera(g_appInput.cams, 2);
-    if(displayGUI)
-      app->DrawFrame(static_cast<float>(thisTime), DrawMode::WITH_GUI);
+    if (displayGUI)
+      app->DrawFrame(static_cast<float>(thisTime), static_cast<float>(diffTime), DrawMode::WITH_GUI);
     else
-      app->DrawFrame(static_cast<float>(thisTime), DrawMode::NO_GUI);
+      app->DrawFrame(static_cast<float>(thisTime), static_cast<float>(diffTime), DrawMode::NO_GUI);
 
     // count and print FPS
     //
     avgTime += diffTime;
     avgCounter++;
-    if(avgCounter >= NAverage)
+    if (avgCounter >= NAverage)
     {
-      auto title = "test";//app->GetWindowTitle();
+      auto title = "test";// app->GetWindowTitle();
       std::stringstream strout;
-      strout << "FPS = " << int( 1.0/(avgTime/double(NAverage)) ) << " " << title;
+      strout << "FPS = " << int(1.0 / (avgTime / double(NAverage))) << " " << title;
 
       glfwSetWindowTitle(window, strout.str().c_str());
       avgTime    = 0.0;
